6906a5d8fb6b6216acad244bb8221fc9c2026f48
Carlos Valencia <peluka613@gmail.com>
2020-08-16 13:27:44 -0500

    Create a dummy users-api as a discovery client

---
+++ spring-boot-microservices/spring-boot-users-api/src/main/java/spring/boot/microservices/usersapi/controllers/UserController.java
@@ -0,0 +1,15 @@
+package spring.boot.microservices.usersapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public String getUsers(){
+        return "Users controller";
+    }
+}
---
+++ spring-boot-microservices/spring-boot-users-api/src/test/java/spring/boot/microservices/usersapi/SpringBootUsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.microservices.usersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUsersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
