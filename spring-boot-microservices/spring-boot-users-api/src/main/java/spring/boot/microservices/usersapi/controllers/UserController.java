6ad33fc40a87ad3ae8076366541e7c04fa05170c
Carlos Valencia <peluka613@gmail.com>
2020-08-17 10:35:31 -0500

    Enable Ribbon Load Balancer

--- spring-boot-microservices/spring-boot-users-api/src/main/java/spring/boot/microservices/usersapi/controllers/UserController.java
+++ spring-boot-microservices/spring-boot-users-api/src/main/java/spring/boot/microservices/usersapi/controllers/UserController.java
@@ -1,5 +1,7 @@
 package spring.boot.microservices.usersapi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping
     public String getUsers(){
-        return "Users controller";
+        return "Users controller: " + env.getProperty("local.server.port");
     }
 }
